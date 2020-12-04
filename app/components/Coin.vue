04c06670f9d457f2c474af4710db51e446454c4c
NingJingwen <20440456@life.hkbu.edu.hk>
2020-12-04 15:36:50 +0800

    slight modification

--- app/components/Coin.vue
+++ app/components/Coin.vue
@@ -1,37 +1,35 @@
 <template>
-    <Page>
-        <StackLayout>
-            <ListView for="restaurant in tappedCoin" @itemTap="onItemTap">
-                <v-template>
-                    <StackLayout orientation="vertical">
-                        <Label :text="restaurant.Restaurant" class="h2" />
-                    </StackLayout>
-                </v-template>
-            </ListView>
+  <Page>
+    <ListView for="restaurant in tappedCoin" @itemTap="onItemTap">
+      <v-template>
+        <StackLayout orientation="vertical">
+          <Label :text="restaurant.Restaurant" class="h2" />
         </StackLayout>
-    </Page>
+      </v-template>
+    </ListView>
+  </Page>
 </template>
 
 <script>
-    import CouponsDetail from "./CouponsDetail";
-    export default {
-        methods: {
-            onItemTap: function(args) {
-                console.log("Item with index: " + args.index + " tapped");
-                console.log("Restaurant tapped: " + args.item.Restaurant);
-                this.$navigateTo(CouponsDetail, {
-                    transition: {},
-                    props: {
-                        tappedProduct: args.item
-                    }
-                });
-            }
+import CouponsDetail from "./CouponsDetail";
+export default {
+  methods: {
+    onItemTap: function (args) {
+      console.log("Item with index: " + args.index + " tapped");
+      console.log("Restaurant tapped: " + args.item.Restaurant);
+      this.$navigateTo(CouponsDetail, {
+        transition: {},
+        props: {
+          tappedProduct: args.item,
         },
-        props: ["tappedCoin"],
-        data() {
-            return {};
-        }
-    };
+      });
+    },
+  },
+  props: ["tappedCoin"],
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style>
--- app/components/MyRedeemedCoupons.vue
+++ app/components/MyRedeemedCoupons.vue
@@ -1,49 +1,46 @@
 <template>
-    <Page>
-        <StackLayout>
-            <ListView for="restaurant in coupons" @itemTap="onItemTap">
-                <v-template>
-                    <StackLayout orientation="vertical">
-                        <Label :text="restaurant.Restaurant" class="h2"/>
-                    </StackLayout>
-                </v-template>
-            </ListView>
+  <Page>
+    <ListView for="restaurant in coupons" @itemTap="onItemTap">
+      <v-template>
+        <StackLayout orientation="vertical">
+          <Label :text="restaurant.Restaurant" class="h2" />
         </StackLayout>
-    </Page>
+      </v-template>
+    </ListView>
+  </Page>
 </template>
 
 <script>
-    import CouponsDetail from "./CouponsDetail";
-    export default {
-        methods: {
-            onItemTap: function(args) {
-                console.log("Item with index: " + args.index + " tapped");
-                console.log("Restaurant tapped: " + args.item.Restaurant);
-                this.$navigateTo(CouponsDetail, {
-                    transition: {},
-                    props: {
-                        tappedProduct: args.item
-                    }
-                });
-            }
+import CouponsDetail from "./CouponsDetail";
+export default {
+  methods: {
+    onItemTap: function (args) {
+      console.log("Item with index: " + args.index + " tapped");
+      console.log("Restaurant tapped: " + args.item.Restaurant);
+      this.$navigateTo(CouponsDetail, {
+        transition: {},
+        props: {
+          tappedProduct: args.item,
         },
-        async mounted() {
-            var response = await fetch(
-                global.baseUrl + "/person/MyRedeemedCoupons"
-            );
-            if (response.ok) {
-                this.coupons = await response.json();
-                console.log(JSON.stringify(this.coupons));
-            } else {
-                console.log(response.statusText);
-            }
-        },
-        data() {
-            return {
-                coupons: []
-            };
-        }
+      });
+    },
+  },
+  async mounted() {
+    var response = await fetch(global.baseUrl + "/person/MyRedeemedCoupons");
+    if (response.ok) {
+      var temp = await response.json();
+      this.coupons = temp.coupons;
+      console.log(JSON.stringify(this.coupons));
+    } else {
+      console.log(response.statusText);
+    }
+  },
+  data() {
+    return {
+      coupons: [],
     };
+  },
+};
 </script>
 
 <style>
